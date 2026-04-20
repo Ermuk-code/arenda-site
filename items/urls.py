89c20f71d1655d507cd0a080c1a418e840bf0d56
Ermuk-code <prectosic@gmail.com>
2026-04-20 21:15:16 +0300

    для изображения

--- items/urls.py
+++ items/urls.py
@@ -1,11 +1,13 @@
 from rest_framework.routers import DefaultRouter
-from .views import ItemViewSet
+from .views import ItemViewSet, ItemImageUploadView
 from django.urls import path
-from .views import ItemImageUploadView
 
 router = DefaultRouter()
 router.register(r'', ItemViewSet, basename='items')
 
-urlpatterns = router.urls + [
+router = DefaultRouter()
+router.register(r'', ItemViewSet, basename='items')
+
+urlpatterns = [
     path('upload-image/', ItemImageUploadView.as_view(), name='upload-image'),
-]
+] + router.urls
