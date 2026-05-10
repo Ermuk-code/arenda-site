692bfe820b3e3f218fd9a470ca7fa9d71df37176
Ermuk-code <86634006+Ermuk-code@users.noreply.github.com>
2026-05-10 15:09:47 +0300

    Add forgot-password flow with email verification code

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from .views import RegisterView
 from .views import ProfileView
 from .views import ChangePasswordView
+from .views import PasswordResetRequestView
+from .views import PasswordResetConfirmView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view()),
     path('change-password/', ChangePasswordView.as_view()),
+    path('password-reset/request/', PasswordResetRequestView.as_view()),
+    path('password-reset/confirm/', PasswordResetConfirmView.as_view()),
 ]
