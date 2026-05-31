49c7de271696eb6a1efd0c7e08f007ac0757b655
Ermuk-code <prectosic@gmail.com>
2026-05-31 15:22:56 +0300

    del postoffices

--- postoffices/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PostOfficesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'postoffices'
-    verbose_name = 'Отделения Почты России'
--- postoffices/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PostOfficeSearchView, PostOfficeDetailView
-
-urlpatterns = [
-    path('search/', PostOfficeSearchView.as_view(), name='post-offices-search'),
-    path('<str:postal_code>/', PostOfficeDetailView.as_view(), name='post-office-detail'),
-]
